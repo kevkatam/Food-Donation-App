bba2ba3c872397cf9a3d1c8b710ffb5a0103a37e
kevkatam <kevkatam@gmail.com>
2024-05-23 16:04:41 +0300

    adding some operations for recipient.py model

--- app/models/recipient.py
+++ app/models/recipient.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from bson.objectid import ObjectId
+from app.database import recipient_collection
 """
 recipient module
 """
@@ -22,3 +24,20 @@ class Recipient(RecipientBase):
     class Config:
         """pydantic configuration for recipient """
         from_attributes = True
+
+
+def recipient_helper(recipient) -> dict:
+    """Helper function to transform recipient document into dictionary"""
+    return {
+        "id": str(recipient["_id"]),
+        "name": recipient["name"],
+        "user_id": recipient["user_id"],
+    }
+
+
+async def create_recipient(recipient: RecipientCreate, user_id: str):
+    """Create a new recipient"""
+    recipient_dict = recipient.dict()
+    recipient_dict['user_id'] = user_id
+    new_recipient = await recipient_collection.insert_one(recipient_dict)
+    return recipient_helper(await recipient_collection.find_one({"_id": new_recipient.inserted_id}))
