51637ea6dc20e22b56744ad05349b54d9f60913a
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-30 07:38:39 +0300

    new database connection

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB conncetion details
-MONGO_DETAILS = "mongodb+srv://kyalokimeu0:vhxyh4oRxn2Mc7De@cluster0.tzweqoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DETAILS = "mongodb+srv://kyalo:root@cluster0.trlij3n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Establish connection to MongoDB server
 client = AsyncIOMotorClient(MONGO_DETAILS)
